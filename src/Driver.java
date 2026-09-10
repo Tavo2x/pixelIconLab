36a5d8c284b0f8859368411d2d3a8cd5398385f8
Tavo2x <131424278+Tavo2x@users.noreply.github.com>
2026-09-10 10:10:39 -0500

    Implanting Pixel class
    - setters, getters, returns rgb string
    - tester in the driver

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,9 @@
 public class Driver {
     public static void main(String[] args) {
         System.out.println("Pixel Icon");
+
+        Pixel p1;
+        p1 = new Pixel(122, 33, 44);
+        System.out.println(p1.toString());
     }
 }
---
+++ src/Pixel.java
@@ -0,0 +1,44 @@
+public class Pixel {
+
+    private int red;
+    private int green;
+    private int blue;
+
+
+    public String toString(){
+        return "rgb(" + getRed() + ", " + getGreen() + ", " + getBlue() + ")";
+    }
+
+    public Pixel(int r, int g, int b){
+        setRed(r);
+        setGreen(g);
+        setBlue(b);
+    }
+
+    public void setRed(int r){
+        if (r > 0 && r < 255){
+            red = r;
+        }
+    }
+    public int getRed(){
+        return red;
+    }
+
+    public void setGreen(int g){
+        if (g > 0 && g < 255){
+            green = g;
+        }    
+    }
+    public int getGreen(){
+        return green;
+    }
+
+    public void setBlue(int b){
+        if (b > 0 && b < 255){
+            blue = b;
+        }
+    }
+    public int getBlue(){
+        return blue;
+    }
+}
