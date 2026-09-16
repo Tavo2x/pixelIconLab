b2cb6eb8b5bb835a2b305a513cfc3436999ed27d
Tavo2x <131424278+Tavo2x@users.noreply.github.com>
2026-09-16 11:28:35 -0500

    Update Driver.java
    test to see if the #, 0's and caps is added

--- src/Driver.java
+++ src/Driver.java
@@ -2,13 +2,20 @@ public class Driver {
     public static void main(String[] args) {
         System.out.println("Pixel Icon");
 
-        Pixel p1;
-        p1 = new Pixel(5647382);
-        System.out.println(p1.toString());
-        System.out.println(p1.toStringHex());
+        //Pixel p1;
+        //p1 = new Pixel(5647382);
+        //System.out.println(p1.toString());
+        //System.out.println(p1.toStringHex());
 
-        Icon I2;
-        I2 = new Icon();
-        System.out.println(I2.toString());
+        Icon I1;
+        I1 = new Icon();
+        //System.out.println(I1.toString());
+
+        Icon I2 = new Icon(2,2);
+        I2.setRed(0, 0, 225);
+        I2.setGreen(0, 1, 225);
+        I2.setBlue(1, 0, 225);
+
+         System.out.println(I2.toString());
     }
 }
