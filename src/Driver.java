8e09507f55f203f793900786fdd9106765efd3fc
Tavo2x <131424278+Tavo2x@users.noreply.github.com>
2026-09-18 12:09:10 -0500

    New Icon
    Top left red. Top right green. Bottom left blue. Bottom right white

--- src/Driver.java
+++ src/Driver.java
@@ -11,12 +11,20 @@ public class Driver {
         //I1 = new Icon();
         //System.out.println(I1.toString());
 
-        Icon I2 = new Icon(5,5);
-        I2.setRed(0, 0, 225);
-        I2.setGreen(0, 4, 225);
-        I2.setBlue(4, 0, 225);
-        System.out.println(I2.toString());
+        //Icon I2 = new Icon(5,5);
+        //I2.setRed(0, 0, 225);
+        //I2.setGreen(0, 4, 225);
+        //I2.setBlue(4, 0, 225);
+        //System.out.println(I2.toString());
 
-        
+        Icon I3 = new Icon(7,8);
+
+        I3.setPixel(0, 0, 225, 0, 0);
+        I3.setPixel(0, 7, 0, 255, 0);
+        I3.setPixel(6, 0, 0, 0, 255);
+        I3.setPixel(6, 7, 255, 255, 255);
+
+        System.out.println(I3.toString());
+
     }
 }
